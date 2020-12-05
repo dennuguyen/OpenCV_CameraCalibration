5efe4bcd6e340b4d096131adfc6defb6b7a2e79b
jovanni tjuandi <jovanni.tjuandi@gmail.com>
2020-12-06 00:56:15 +1100

    add calibration

--- objectDetect/backup.cpp
+++ objectDetect/backup.cpp
@@ -1,43 +1,43 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/aruco.hpp"
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/core/utility.hpp>
-#include <iostream>
 
 //using namespace cv;
 
 int main(int argc, char** argv)
 {
-    cv::VideoCapture inputVideo;
-inputVideo.open(0);
-cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-while (inputVideo.grab()) {
-    cv::Mat image, imageCopy;
-    inputVideo.retrieve(image);
-    image.copyTo(imageCopy);
-    std::vector<int> ids;
-    std::vector<std::vector<cv::Point2f> > corners;
-    cv::aruco::detectMarkers(image, dictionary, corners, ids);
-     //if at least one marker detected
-    if (ids.size() > 0) {
-        cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
-        std::cout << "marker found" << std::endl;
-    }
-
-    //std::string image_path = cv::samples::findFile("tags.jpg");
-    image = cv::imread( "/home/owr01/OpenCV_CameraCalibration/objectDetect/tags.jpg" );
-    cv::aruco::detectMarkers(image, dictionary, corners, ids);
-if (ids.size() > 0) {
-        //cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
-        std::cout << "marker found" << std::endl;
-    }
-    cv::imshow("out", imageCopy);
-    char key = (char) cv::waitKey(10);
-    if (key == 27)
-        break;
-}
+    cv::VideoCapture cap;
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation.
+    if(!cap.open(0)) 
+        return 0;
 
 
-return 0;
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    for(;;) {
+        cv::Mat frame;
+        cap >> frame;
+
+        cv::Mat copy;
+        frame.copyTo(copy);
+        std::vector<int> ids;
+        std::vector<std::vector<cv::Point2f> > corners;
+        cv::aruco::detectMarkers(frame, dictionary, corners, ids);
+
+        if (ids.size() > 0)
+        cv::aruco::drawDetectedMarkers(copy, corners, ids);
+
+
+        if( frame.empty() ) break; // end of video stream
+            cv::imshow("Find AR", copy);
+        if( cv::waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
+        }
+
+    // the camera will be closed automatically upon exit
+    //cap.close();
+    cap.release();
+    return 0;
+
+
+
 
 }
--- objectDetect/detect.cpp
+++ objectDetect/detect.cpp
@@ -8,28 +8,29 @@ int main(int argc, char** argv)
     cv::VideoCapture cap;
     // open the default camera, use something different from 0 otherwise;
     // Check VideoCapture documentation.
-    if(!cap.open(0)) return 0;
+    if(!cap.open(0)) 
+        return 0;
 
 
     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     for(;;) {
-          cv::Mat frame;
-          cap >> frame;
+        cv::Mat frame;
+        cap >> frame;
 
-          cv::Mat copy;
-          frame.copyTo(copy);
-          std::vector<int> ids;
-          std::vector<std::vector<cv::Point2f> > corners;
-          cv::aruco::detectMarkers(frame, dictionary, corners, ids);
+        cv::Mat copy;
+        frame.copyTo(copy);
+        std::vector<int> ids;
+        std::vector<std::vector<cv::Point2f> > corners;
+        cv::aruco::detectMarkers(frame, dictionary, corners, ids);
 
-          if (ids.size() > 0)
-              cv::aruco::drawDetectedMarkers(copy, corners, ids);
-          
+        if (ids.size() > 0)
+        cv::aruco::drawDetectedMarkers(copy, corners, ids);
 
-          if( frame.empty() ) break; // end of video stream
-          cv::imshow("this is you, smile! :)", copy);
-          if( cv::waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
-    }
+
+        if( frame.empty() ) break; // end of video stream
+            cv::imshow("Find AR", copy);
+        if( cv::waitKey(10) == 27 ) break; // stop capturing by pressing ESC 
+        }
 
     // the camera will be closed automatically upon exit
     //cap.close();
